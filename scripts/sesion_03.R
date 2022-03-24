b75f9f605ec532249f866d5de981d10901b0c103
scidatmath2020 <56654363+scidatmath2020@users.noreply.github.com>
2022-03-24 15:40:46 -0600

    Add files via upload

---
+++ scripts/sesion_03.R
@@ -0,0 +1,80 @@
+exp(2)
+
+## 5^x para valores de x = 0,1,2,3,4,5,6
+
+5^c(0,1,2,3,4,5,6)
+
+exp(c(0,1,2,3,4,5,6))
+
+## a^x
+
+log(125,base=5)
+
+log(5^20,base=5)
+
+
+log(32,base=2)
+
+?log()
+#ln para logaritmo natural; log para logaritmo base 10
+log(exp(23))
+
+# ln(24)+log_17(24)
+
+log(24)+log(24,base=17)
+log(24)+log2(24)
+
+
+### Raíz que no es cuadrado
+
+2022^(1/3)
+
+log(24,base=2)
+log(24,2)
+
+
+#############
+
+(1-3)*8/0
+
+(1+3)*8/0
+
+0/0
+
+(exp(0)-1)/0
+
+sqrt(-4)
+
+sqrt(-4+0i)
+
+NaN / 0
+
+NaN * 2
+
+5 / NaN
+
+mis_personas <- c(71.2,75.4,63,NaN,80)
+
+71.2 + 75.4 + 63 + NaN + 80
+
+2/0 + NaN
+
+?asin()
+
+help(asin)
+
+sin(45*3.1416/180)
+
+sinpi(1/4)
+
+mi_solucion <- sinpi(1/4)
+
+c(10000,20000,4000,"3000")
+
+c(1,2,"hola",3)
+.-
+
+
+
+
+
